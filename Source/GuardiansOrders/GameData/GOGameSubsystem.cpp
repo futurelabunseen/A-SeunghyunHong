b2d2f921ae18f07ba1ece03cc976273f5a8bf01f
strurao <strurao@gmail.com>
2024-04-16 13:48:19 +0900

    Feat: 추가 GameInstanceSubsystem

---
+++ Source/GuardiansOrders/GameData/GOGameSubsystem.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameData/GOGameSubsystem.h"
+
+UGOGameSubsystem::UGOGameSubsystem()
+{
+	CharacterMaxCnt = 0;
+	SkillMaxCnt = 0;
+}
+
+void UGOGameSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	LoadCharacterStats();
+	LoadSkillStats();
+}
+
+void UGOGameSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UGOGameSubsystem::LoadCharacterStats()
+{
+	UDataTable* DataTable = Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, TEXT("/Game/GameData/HeroDataTable/GOCharacterStatTable.GOCharacterStatTable")));
+	if (DataTable)
+	{
+		TArray<uint8*> ValueArray;
+		DataTable->GetRowMap().GenerateValueArray(ValueArray);
+		Algo::Transform(ValueArray, CharacterStatTable, [](uint8* Value)
+			{
+				return *reinterpret_cast<FGOCharacterStat*>(Value);
+			});
+	}
+}
+
+void UGOGameSubsystem::LoadSkillStats()
+{
+	UDataTable* DataTable = Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, TEXT("/Game/GameData/SkillDataTable/GOSkillStatTable.GOSkillStatTable")));
+	if (DataTable)
+	{
+		TArray<uint8*> ValueArray;
+		DataTable->GetRowMap().GenerateValueArray(ValueArray);
+		Algo::Transform(ValueArray, SkillStatTable, [](uint8* Value)
+			{
+				return *reinterpret_cast<FGOSkillStat*>(Value);
+			});
+	}
+}
---
+++ Source/GuardiansOrders/GameData/GOGameSubsystem.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GOCharacterStat.h"
+#include "GOSkillStat.h"
+#include "GOGameSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUARDIANSORDERS_API UGOGameSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+    UGOGameSubsystem();
+
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    // 기능 접근 메서드
+    FORCEINLINE FGOCharacterStat GetCharacterStat(int32 InCType) const { return CharacterStatTable.IsValidIndex(InCType - 1) ? CharacterStatTable[InCType - 1] : FGOCharacterStat(); }
+    FORCEINLINE FGOSkillStat GetSkillStat(int32 InSType) const { return SkillStatTable.IsValidIndex(InSType - 1) ? SkillStatTable[InSType - 1] : FGOSkillStat(); }
+
+protected:
+    // 데이터 테이블 로드
+    void LoadCharacterStats();
+    void LoadSkillStats();
+
+    TArray<FGOCharacterStat> CharacterStatTable;
+    TArray<FGOSkillStat> SkillStatTable;
+
+    int32 CharacterMaxCnt;
+    int32 SkillMaxCnt;
+};
