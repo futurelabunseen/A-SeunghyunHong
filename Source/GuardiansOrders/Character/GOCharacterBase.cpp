1661800ea20de0b9b2a8a003437dd748166e14df
strurao <strurao@gmail.com>
2024-04-21 03:15:20 +0900

    Test: Movement Friction

--- Source/GuardiansOrders/Character/GOCharacterBase.cpp
+++ Source/GuardiansOrders/Character/GOCharacterBase.cpp
@@ -46,6 +46,8 @@ AGOCharacterBase::AGOCharacterBase()
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
 	GetCharacterMovement()->bEnablePhysicsInteraction = false;
+	GetCharacterMovement()->GroundFriction = 8.0f; // 기본 마찰력을 증가시켜 미끄러짐 감소
+	GetCharacterMovement()->BrakingFrictionFactor = 2.0f; // 브레이킹(정지) 마찰력을 증가
 	// GetCharacterMovement()->SlideAlongSurface(false);
 
 	// Mesh
