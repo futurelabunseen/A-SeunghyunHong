620e9756e4e8e77475179e3b17e5f4ef240ca1db
strurao <strurao@gmail.com>
2024-04-24 16:36:13 +0900

    Add: Skill Interfaces

---
+++ Source/GuardiansOrders/Skill/Interface/AttackBloackableInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Skill/Interface/AttackBloackableInterface.h"
+
+// Add default functionality here for any IAttackBloackableInterface functions that are not pure virtual.
