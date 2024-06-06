99ce189a75fd221116dbddde20cc66ea84392032
strurao <strurao@gmail.com>
2024-06-06 14:27:49 +0900

    Feat: Katniss SkillE: Timer multiple Projectiles

--- Source/GuardiansOrders/Skill/Katniss/GOKatnissSkill03.cpp
+++ Source/GuardiansOrders/Skill/Katniss/GOKatnissSkill03.cpp
@@ -2,10 +2,12 @@
 
 
 #include "Skill/Katniss/GOKatnissSkill03.h"
+#include <Skill/GOSkillCastComponent.h>
 
 UGOKatnissSkill03::UGOKatnissSkill03()
 {
-
+	bIsSpreadSkill = false;
+	NumProjectiles = 7;
 }
 
 void UGOKatnissSkill03::PostInitProperties()
@@ -18,6 +20,8 @@ void UGOKatnissSkill03::StartCast()
 	//SetCoolDownTimer();
 	//bIsCasting = true;
 	Super::StartCast();
+	ProjectilesFired = 0;
+	SpawnSequentialProjectiles();
 }
 
 void UGOKatnissSkill03::UpdateCast(float DeltaTime)
@@ -34,11 +38,13 @@ void UGOKatnissSkill03::ActivateSkill()
 void UGOKatnissSkill03::FinishCast()
 {
 	Super::FinishCast();
+	GetWorld()->GetTimerManager().ClearTimer(ProjectileTimerHandle);
 }
 
 void UGOKatnissSkill03::InterruptedCast()
 {
 	Super::InterruptedCast();
+	GetWorld()->GetTimerManager().ClearTimer(ProjectileTimerHandle);
 }
 
 void UGOKatnissSkill03::ActivateEffect()
@@ -46,6 +52,34 @@ void UGOKatnissSkill03::ActivateEffect()
 	Super::ActivateEffect();
 }
 
+void UGOKatnissSkill03::SpawnSequentialProjectiles()
+{
+	if (ProjectilesFired < NumProjectiles)
+	{
+		if (GetSkillOwner())
+		{
+			FVector Location = GetSkillOwner()->GetActorLocation();
+			FRotator Rotation = GetSkillOwner()->GetActorRotation();
+
+			// Introduce a slight random variation in the rotation
+			float RandomYawOffset = FMath::FRandRange(-10.0f, 10.0f); // Adjust the range as needed for more or less variation
+			Rotation.Yaw += RandomYawOffset;
+
+			SpawnProjectile(Location, Rotation);
+
+			ProjectilesFired++;
+			GetWorld()->GetTimerManager().SetTimer(ProjectileTimerHandle, this, &UGOKatnissSkill03::SpawnSequentialProjectiles, 0.1f, false);
+		}
+	}
+}
+
+void UGOKatnissSkill03::SpawnProjectile(FVector Location, FRotator Rotation)
+{
+	if (UGOSkillCastComponent* SkillCastComponent = GetSkillOwner()->FindComponentByClass<UGOSkillCastComponent>())
+	{
+		SkillCastComponent->HandleProjectileSkill(this, Location, Rotation, FTransform(Rotation, Location));
+	}
+}
 //bool UGOKatnissSkill03::IsCasting() const
 //{
 //	return false;
